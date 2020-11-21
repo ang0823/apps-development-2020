51d8dac13a5278b326404d2a642e4412941ee73d
Angel Sanchez <angelsanchez934@gmail.com>
2020-11-20 18:45:19 -0600

    Cambio de carpeta
    Se movio el fragmento de la panalla de favoritos a una carpeta interior llamada Fragment

--- SocialBox/app/src/main/java/com/desapp/socialbox/fragment/FavoritesFragment.java
+++ SocialBox/app/src/main/java/com/desapp/socialbox/fragment/FavoritesFragment.java
@@ -1,4 +1,4 @@
-package com.desapp.socialbox;
+package com.desapp.socialbox.fragment;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.desapp.socialbox.R;
 
 /**
  * A simple {@link Fragment} subclass.
