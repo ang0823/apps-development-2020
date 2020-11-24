0466b8dcba9245c90388de56904cbf3a2364d232
Angel Sanchez <angelsanchez934@gmail.com>
2020-11-23 19:36:27 -0600

    Nuevos metodos
    Se agregaron los metodos "iniciarSesion" y "crearCuenta" que son usados por los botones con los mismos nombre en el main activity

--- SocialBox/app/src/main/java/com/desapp/socialbox/MainActivity.java
+++ SocialBox/app/src/main/java/com/desapp/socialbox/MainActivity.java
@@ -2,13 +2,27 @@ package com.desapp.socialbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main );
+    }
+
+    public void iniciarSesion(View vista){
+        Intent actividad = new Intent(this, NavigationActivity.class);
+        startActivity(actividad);
+        this.finish();
+    }
+
+    public void crearCuenta(View vista){
+        Intent actividad = new Intent(this, SignupActivity.class);
+        startActivity(actividad);
     }
 }
