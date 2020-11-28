435bc70e7bf6d40f750fa713e86f4c9b21922624
Angel Sanchez <angelsanchez934@gmail.com>
2020-11-28 11:31:20 -0600

    NUEVO ENDPOINT AGREGADO
    Se finalizo el endpoint para buscar a un usuario por correo electronico. Adicionalmente, se modificaron las rutas de los endpoints que buscan porque entraba em coflicto.

--- SocialBox_API/routes/usuarios.js
+++ SocialBox_API/routes/usuarios.js
@@ -4,16 +4,15 @@ const Usuario = require("../database/models/Usuario")
 const {Op} = require('sequelize')
 
 // EndPonit para obtener cuentas por nombre
-router.get('/:nombre', (req, res) => {
+router.post('/nombre', (req, res) => {
     Usuario.findAll({
         attributes: ['nombre', 'apellidos', 'email'],
         where: {
             nombre: {
-                [Op.like]: '%' + req.params.nombre + '%'
+                [Op.like]: '%' + req.body.nombre + '%'
             }
         }
     }).then(users => {
-        console.log(users.length)
         if(users.length > 0) {
             res.json(users);
         } else {
@@ -22,11 +21,31 @@ router.get('/:nombre', (req, res) => {
             })
         }
     }).catch(error => {
-        res.json(error)
+        console.log(error)
+        res.json({
+            response: 'Ocurrio un error, intente más tarde.'
+        })
     })
 })
 
 // EndPonit para obtener una cuenta por email
+router.post('/correo', (req, res) => {
+    Usuario.findAll({
+        attributes: ['nombre', 'apellidos', 'email'],
+        where: {
+            email: req.body.email
+        }
+    }).then(user => {
+        res.json({
+            user
+        })
+    }).catch(error => {
+        console.log(error)
+        res.json({
+            response: 'Ocurrio un error, intente más tarde.'
+        })
+    })
+})
 
 // EndPonit para registrar una cuenta
 router.post('/', (req, res) => {
