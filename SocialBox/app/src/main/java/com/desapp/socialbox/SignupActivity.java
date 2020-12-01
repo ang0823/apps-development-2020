776b685e0c27dde829eb4d763b1a288850436263
Angel Sanchez <angelsanchez934@gmail.com>
2020-12-01 10:38:15 -0600

    DEPENDECIA JWT AGREGADA
    Se hara uso de esta dependencia para generar token y la autenticacon

--- SocialBox/app/src/main/java/com/desapp/socialbox/SignupActivity.java
+++ SocialBox/app/src/main/java/com/desapp/socialbox/SignupActivity.java
@@ -6,12 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class SignupActivity extends AppCompatActivity {
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        queue = Volley.newRequestQueue(this);
     }
 
     public void cancelarRegistro(View view){
