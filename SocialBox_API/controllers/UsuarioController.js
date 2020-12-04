72d7b401950cc70488085b1c9294786a90a573f2
Angel Sanchez <angelsanchez934@gmail.com>
2020-12-03 23:19:40 -0600

    Nuevos metodos
    Se agregaron los metodos que  crean y actuaizan a un usuario, otro para cambiar foto de perfil. Adicionalmente se agregaron los atributos PROFILEPIC y STATUS para recuperar la foto de perfil y estados de las cyentas.

--- SocialBox_API/controllers/UsuarioController.js
+++ SocialBox_API/controllers/UsuarioController.js
@@ -1,12 +1,12 @@
 const Usuario = require('../database/models/Usuario')
-const {Op} = require('sequelize')
+const {Op, where} = require('sequelize')
 
 
 
 module.exports = {
     findByName: function (name, callback) {
         Usuario.findAll({
-            attributes: ['nombre', 'apellidos', 'email'],
+            attributes: ['nombre', 'apellidos', 'email', 'profilePic', 'status'],
             where: {
                 nombre: {
                     [Op.like]: '%' + name + '%'
@@ -21,7 +21,7 @@ module.exports = {
 
     findByEmail: function (email, callback) {
         Usuario.findOne({
-            attributes: ['nombre', 'apellidos', 'email'],
+            attributes: ['nombre', 'apellidos', 'email', 'profilePc', 'status'],
             where: {
                 email: email
             }
@@ -30,5 +30,46 @@ module.exports = {
         }).catch(error => {
             callback(error)
         });
+    },
+
+    create: function(user, callback) {
+        Usuario.create({
+            nombre: user.nombre,
+            apellidos: user.apellidos,
+            email: user.email,
+            contrasena: user.contrasena
+        }).then(user => {
+            callback(null, `${user.nombre} ${user.apellidos} ${user.email}`)
+        }).catch(error => {
+            callback(error)
+        });
+    },
+
+    update: function(user, callback) {
+            Usuario.update({           
+                nombre: user.nombre,
+                apellidos: user.apellidos,
+                status: user.status}, {
+                    where: {
+                        email: user.email
+                    }
+                }).then(result => {
+                callback(null, result)
+            }).catch(error => {
+                callback(error)
+            });
+    },
+
+    updateProfPic: function(datos, callback) {
+        Usuario.update({
+            profilePic: "./images/profile/" + datos.imageName}, {
+                where: {
+                    email: datos.userEmail
+                }
+            }).then(result => {
+                callback(result)
+            }).catch(error => {
+                callback(error)
+            })
     }
 }
