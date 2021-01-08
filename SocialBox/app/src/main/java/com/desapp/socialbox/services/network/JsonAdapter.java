bd130cacecda66073e5ace3e6f97805f2a4d9c45
Angel Sanchez <angelsanchez934@gmail.com>
2021-01-07 19:31:37 -0600

    SEAGREGO METODO SEARCHADAPTER
    Usado para convertir todos los usuarios regresados a objetos de java

--- SocialBox/app/src/main/java/com/desapp/socialbox/services/network/JsonAdapter.java
+++ SocialBox/app/src/main/java/com/desapp/socialbox/services/network/JsonAdapter.java
@@ -1,11 +1,18 @@
 package com.desapp.socialbox.services.network;
 
+import android.util.Log;
+
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.desapp.socialbox.models.pojos.Usuario;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class JsonAdapter {
 
@@ -17,5 +24,25 @@ public class JsonAdapter {
         response.setUsername(jsonObject.getString("username"));
 
         return response;
+    }
+
+    public static ArrayList<Usuario> SearchAdapter(JSONObject response) throws JSONException {
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        JSONArray usersData = response.getJSONArray("usuarios");
+
+        for(int i = 0; i < usersData.length(); i++) {
+            Usuario usuario = new Usuario();
+            JSONObject jsonObject = usersData.getJSONObject(i);
+
+            usuario.setId(jsonObject.getInt("id"));
+            usuario.setNombre(jsonObject.getString("nombre"));
+            usuario.setApellidos(jsonObject.getString("apellidos"));
+            usuario.setUsername(jsonObject.getString("username"));
+            usuario.setProfilePic(jsonObject.getString("profilePic"));
+            usuario.setStatus(jsonObject.getString("status"));
+            usuarios.add(usuario);
+        }
+
+        return usuarios;
     }
 }
